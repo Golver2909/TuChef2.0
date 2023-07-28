75b3f34327b3c0855d578734500c5e6fbe8d4ceb
Matias Fuentes <matiasfuentes2909@gmail.com>
2023-07-28 09:33:48 -0300

    Añado el componente navbar al modulo de shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
