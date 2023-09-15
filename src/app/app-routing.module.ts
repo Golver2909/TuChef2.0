23862be0fee73ba1a53a4f28e579754030c7cf36
garciacarrera <fxtr088@gmail.com>
2023-09-15 10:36:56 -0300

    creacion de modulo register y componente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ const routes: Routes = [
 },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'register', loadChildren: () => import('./auth/register/register.module').then(m => m.RegisterModule) },
 
 ];
 
