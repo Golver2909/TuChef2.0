e2f82c3bb547f40eef5cbdcfe59522c486903ca8
garciacarrera <fxtr088@gmail.com>
2023-09-15 18:32:13 -0300

    Implementado formulario de registro en el componente RegisterComponent con campos de email y contraseña

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {FormGroup, FormControl } from '@angular/forms'
 /*
+ import {FormGroup, FormControl } from '@angular/forms'
  se utiliza para crear un grupo de controles de formulario que 
  pueden representar una sección o un formulario completo en una 
  aplicación 
@@ -12,5 +13,8 @@ import {FormGroup, FormControl } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+registerForm = new  FormGroup({
+  email: new FormControl (''),
+  password: new FormControl (''),
+})
 }
