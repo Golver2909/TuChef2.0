4a5bdf696cd1156715e7589cc1b33b8fb690fc0b
garciacarrera <fxtr088@gmail.com>
2023-09-17 13:39:37 -0300

    Implementado un formulario de inicio de sesión en Angular utilizando FormGroup y FormControl para gestionar los campos de email y contraseña

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  loginForm= new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 }
