023c659364a9bff6541be92d90dd23c1351be37d
Brunosqui14 <brunoquezada41>
2023-09-22 11:15:33 -0300

    tabla hecha sin erroes

--- src/app/modules/pages/crear-receta/crear-receta.component.ts
+++ src/app/modules/pages/crear-receta/crear-receta.component.ts
@@ -1,31 +1,38 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {NgIf} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatListModule} from '@angular/material/list';
+import { PeriodicElement } from 'src/app/models/periodicElement ';
+ 
 
 
 @Component({
   selector: 'app-crear-receta',
   templateUrl: './crear-receta.component.html',
   styleUrls: ['./crear-receta.component.css'],
-  standalone: true,
-  imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    NgIf,
-    MatButtonModule,
-    MatIconModule,
-    MatChipsModule,
-    MatListModule
-  ],
+
+
 })
-export class CrearRecetaComponent {}
+
+
+
+export class CrearRecetaComponent {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+
+
 
 
 
--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -4,17 +4,47 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { CrearRecetaComponent } from './crear-receta/crear-receta.component';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatListModule} from '@angular/material/list';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable, ReplaySubject} from 'rxjs';
+import {MatTableModule} from '@angular/material/table';
+
 
 @NgModule({
   declarations: [
+    CrearRecetaComponent
   ],
   imports: [
     CommonModule,
     PagesRoutingModule,
-    CrearRecetaComponent
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatListModule,
+    MatTableModule
   ],
   exports:[
-    CrearRecetaComponent
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatListModule,
+    MatTableModule
   ]
 })
 export class PagesModule { }
