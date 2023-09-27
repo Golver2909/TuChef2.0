a5d8af9e5eb9215141f747d72702aedf550496a6
emmanuel <crackseba201@gmail.com>
2023-09-27 13:11:42 -0300

    crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,18 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   //aplicando carga perezosa en las rutas
 
+
+
+
+
+
+
+
+
+
+
+
+
   {
     path: '',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
