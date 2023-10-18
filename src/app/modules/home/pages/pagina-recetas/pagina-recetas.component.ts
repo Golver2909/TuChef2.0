0f9126c0072244c63ee9414a5051d822c9cb448d
Matias Fuentes <matiasfuentes2909@gmail.com>
2023-10-18 08:56:39 -0300

    Componente de pagina-recetas y modulos home creados

---
+++ src/app/modules/home/pages/pagina-recetas/pagina-recetas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-recetas',
+  templateUrl: './pagina-recetas.component.html',
+  styleUrls: ['./pagina-recetas.component.css']
+})
+export class PaginaRecetasComponent {
+
+}
