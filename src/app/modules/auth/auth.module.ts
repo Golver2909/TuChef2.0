ce7bcb235c0ab548435c05036bf7bb41a8222390
garciacarrera <fxtr088@gmail.com>
2023-10-19 16:31:52 -0300

    creacion del componente login

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { LoginComponent } from './login/login/login.component';
 
 
 @NgModule({
   declarations: [
     
+  
+    LoginComponent
   ],
   imports: [
     CommonModule,
