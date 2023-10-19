43717343413a2ef3b333c2589d7628b930572553
garciacarrera <fxtr088@gmail.com>
2023-10-19 17:38:04 -0300

    Se ha añadido un formulario de registro en el componente RegisterComponent, que permite a los usuarios ingresar su correo electrónico y contraseña. Cuando se envía el formulario, se llama al método de registro del servicio AuthService para crear una cuenta de usuario con las credenciales proporcionadas

--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // Importación del decorador Component de Angular
+import { FormGroup, FormControl } from '@angular/forms'; // Importación de FormGroup y FormControl para trabajar con formularios
+import { AuthService } from '../services/auth.service'; // Importación del servicio AuthService
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: 'app-register', // Selector del componente
+  templateUrl: './register.component.html', // Plantilla HTML asociada al componente
+  styleUrls: ['./register.component.css'], // Estilos CSS asociados al componente
+  providers: [AuthService] // Proveedor del servicio AuthService
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({ // Creación de un formulario de grupo llamado registerForm
+    email: new FormControl(), // Campo de formulario para el correo electrónico
+    password: new FormControl() // Campo de formulario para la contraseña
+  });
 
+  constructor(private servicioAuth: AuthService) {} // Constructor del componente con inyección de dependencia del servicio AuthService
+
+  onRegister() { // Método para registrar un usuario
+    const { email, password } = this.registerForm.value; // Obtención de los valores de correo electrónico y contraseña del formulario
+    this.servicioAuth.register(email, password); // Llamada al método de registro del servicio AuthService
+  }
 }
