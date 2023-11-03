b72fc6781ebbf4ebe9e3a2f0b6c272df04e6fb53
Matias Fuentes <matiasfuentes2909@gmail.com>
2023-11-03 09:05:51 -0300

    interfaz de usuario y service de navbar con imports agregados

---
+++ src/app/shared/services/nav.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NavService {
+
+
+
+  constructor() { }
+}
