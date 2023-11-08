798e958a7cefa6a36ee7db38f6c75a62b53c3f73
Golver2909 <111146183+Golver2909@users.noreply.github.com>
2023-11-08 10:23:54 -0300

    Actualizar imports app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';//import { AngularFireStorageModule } from "@angular/fire/compat/storage";
+import { AppComponent } from './app.component';//import { environment } from 'src/environments/environment';import { AngularFireModule } from "@angular/fire/compat";import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 
-
-
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 
 @NgModule({
   declarations: [
