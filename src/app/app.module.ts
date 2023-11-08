5ede9ab319099ca3936310b9d98d4c6c41048b8b
Golver2909 <111146183+Golver2909@users.noreply.github.com>
2023-11-08 09:43:45 -0300

    Actualizar app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
@@ -12,6 +14,8 @@ import { AngularFireStorageModule } from "@angular/fire/compat/storage";
 @NgModule({
   declarations: [
     AppComponent
+
+    
   ],
   imports: [
     BrowserModule,
