09bd887777df9e4e000cf6ac70514cbb9a29dd43
Brunosqui14 <brunoquezada41@gmail.com>
2023-11-10 09:36:54 -0300

    creacion de carpeta para mostrar las recetas

---
+++ src/app/modules/pages/mostrar-receta/mostrar-receta.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-receta',
+  templateUrl: './mostrar-receta.component.html',
+  styleUrls: ['./mostrar-receta.component.css']
+})
+export class MostrarRecetaComponent {
+
+}
--- src/app/modules/pages/pages.module.ts
+++ src/app/modules/pages/pages.module.ts
@@ -5,10 +5,12 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { CardsComponent } from './cards/cards.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MostrarRecetaComponent } from './mostrar-receta/mostrar-receta.component';
 
 @NgModule({
   declarations: [
-    CardsComponent
+    CardsComponent,
+    MostrarRecetaComponent
   ],
   imports: [
     CommonModule,
