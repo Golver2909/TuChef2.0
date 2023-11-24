a1cb4d0095a558d6fb72221b08fb3e8c7b03aa4f
garciacarra <fxtr088@gmail.com>
2023-11-24 09:33:13 -0300

    realizo importaciones en el service

--- src/app/modules/admin/service/crud.service.ts
+++ src/app/modules/admin/service/crud.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-
+import { Receta } from 'src/app/models/receta';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
