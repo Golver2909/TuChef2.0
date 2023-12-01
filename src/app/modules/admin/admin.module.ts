78db4c46a0a44541bc8202650722a1fa877891ac
garciacarrera <fxtr088@gmail.com>
2023-12-01 14:01:55 -0300

    creo el component/table para guardar los datos de las recetas y poder eliminarlas y editarlas

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TableComponent
+  ],
   exports:[],
   imports: [
     CommonModule,
