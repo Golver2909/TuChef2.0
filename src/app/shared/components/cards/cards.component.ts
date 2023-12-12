aa3b0358a832f05dfb18ad49982387bc044a8343
NotMuuns <santicipo21@gmail.com>
2023-12-12 12:01:09 -0300

    cree la rama para comentar mi codigo de cards

--- src/app/shared/components/cards/cards.component.ts
+++ src/app/shared/components/cards/cards.component.ts
@@ -1,3 +1,4 @@
+/*importes del modelo receta, del metodo OnInit y del servicio Crud*/
 import { Component, OnInit } from '@angular/core';
 import { Receta } from 'src/app/models/receta';
 import { CrudService} from "src/app/modules/admin/service/crud.service";
@@ -5,20 +6,23 @@ import { CrudService} from "src/app/modules/admin/service/crud.service";
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
+
 })
 export class CardsComponent {
+  /*Se crea la coleccion receta para almacenar recetas*/
   coleccionRecetas:Receta []=[]
 
 
-
+  /*Se aclara que se esperan los datos cargados*/
   RecetaSeleccionada!:Receta;
 
 
-  
+/*El servicio crud es capaz de usarse en otros ambitos del proyecto*/
   constructor(public servicioCrud: CrudService){
-  
+
   }
 
+  /*El OnInit inicializa para obtener datos Receta*/
   ngOnInit(): void{
     this.servicioCrud.obtenerRecetas().subscribe(Reteta=>{
       this.coleccionRecetas=Reteta;
@@ -28,11 +32,12 @@ export class CardsComponent {
   }
 
 
+  
   mostrarVer(info: Receta){
     // al seleccionar "ver más" el modal visible pasa a "true"
-    
+
     // muestra la información del producto que se seleccione
     this.RecetaSeleccionada = info;
   }
- 
+
 }
